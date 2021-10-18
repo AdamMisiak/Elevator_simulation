bf30380cd0318c7a2245a22f56a00f1e64416bf3
Adam <adammisiak3@gmail.com>
2021-10-18 16:53:48 +0200

    Added elevator object

---
+++ src/components/Elevator.js
@@ -0,0 +1,15 @@
+import '../styles/Elevator.css';
+
+import React from 'react';
+
+const Elevator = ({}) => {
+    
+    return(
+        <div className="elevator">
+            
+        </div>
+
+    )   
+}
+
+export default Elevator;
