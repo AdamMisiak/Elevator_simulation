a682b03e6dd4d85b1f42955eea926a367410225b
Adam <adammisiak3@gmail.com>
2021-10-19 19:43:26 +0200

    Added types

---
+++ src/reducers/positionReducer.js
@@ -0,0 +1,14 @@
+import { CHANGE_POSITION } from '../actions/types';
+
+const initialState = {
+    items: [],
+    item: {}
+};
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+        default: 
+            return state;
+    }
+
+}
