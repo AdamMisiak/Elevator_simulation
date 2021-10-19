66d9265c5017a42f647f27586564515477d9f6ea
Adam <adammisiak3@gmail.com>
2021-10-19 19:30:02 +0200

    Added basic reducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './styles/App.css';
 
-import { applyMiddleware, createStore } from 'redux';
 import { useEffect, useRef, useState } from 'react';
 
 import Elevator from './components/Elevator'
 import { Provider } from 'react-redux';
-
-const store = createStore(() => [], {}, applyMiddleware());
+import store from './store'
 
 function App() {
   return (
