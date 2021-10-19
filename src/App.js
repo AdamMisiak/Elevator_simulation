bdea19a15881506c0a8c4f49e3d85fbfe29d012b
Adam <adammisiak3@gmail.com>
2021-10-19 19:12:00 +0200

    Added redux store and provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './styles/App.css';
 
+import { applyMiddleware, createStore } from 'redux';
 import { useEffect, useRef, useState } from 'react';
 
 import Elevator from './components/Elevator'
+import { Provider } from 'react-redux';
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 function App() {
-
   return (
-    <div className="App">
-      <Elevator/>     
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Elevator/>     
+      </div>
+    </Provider>
   );
 }
 
