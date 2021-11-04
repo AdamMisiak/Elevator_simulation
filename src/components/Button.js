9df783a97d0ae1dd7e3e4cdb62a660d9f2ee39af
Adam <adammisiak3@gmail.com>
2021-11-04 16:56:59 +0100

    Added levels y positions

--- src/components/Button.js
+++ src/components/Button.js
@@ -12,7 +12,6 @@ const Button = ({
     
     function HandleOrderElevator(e){
         dispatch(orderElevator(level));
-
     }
 
     return(
--- src/components/Level.js
+++ src/components/Level.js
@@ -1,13 +1,24 @@
 import '../styles/Level.css';
 
+import { useRef, useState } from 'react';
+
 import Button from './Button'
 import React from 'react';
 
 const Level = (object) => { 
+    const ref = useRef(null)
+    const [position, setPosition] = useState(0);
+
+    if (ref.current !== null) {
+        // setPosition(ref.current.getBoundingClientRect().top)
+        console.log(ref.current.getBoundingClientRect().top)
+    }
+
     return(
         <div className="level-container">
-            <div className="level">
+            <div ref={ref} className="level">
                 LEVEL: {object.id}
+                <p>POSITION: {position}</p>
                 <Button 
                     label="Order"
                     level={object.id}
